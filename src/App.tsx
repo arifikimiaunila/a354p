e78cd2035ca71855b5b76d43fd4512779ce6d759
arifikimiaunila <arifikimiaunila@gmail.com>
2024-06-29 12:27:29 +0700

    Create App.tsx

---
+++ src/App.tsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import './dropdown scss/drop.scss'
+
+function App() {
+    const [myCar, setMyCar] = useState("Volvo");
+
+    const handleChange = (event) => {
+        setMyCar(event.target.value)
+    }
+
+  return (
+    <>
+          <form>
+              <select value={myCar} onChange={handleChange}>
+                  <option value="Ford">Ford</option>
+                  <option value="Volvo">Volvo</option>
+                  <option value="Fiat">Fiat</option>
+              </select>
+          </form>
+    </>
+  )
+}
+
+export default App
